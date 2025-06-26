621757c0cefcbb50eb9eaaab25280d4075861315
nikityanka <zed5847@gmail.com>
2025-06-26 18:44:34 +0300

    Add files via upload

---
+++ BookForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace BookRatingApp
+{
+    partial class BookForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.cmbAuthors = new System.Windows.Forms.ComboBox();
+            this.lblPublicationYear = new System.Windows.Forms.Label();
+            this.numPublicationYear = new System.Windows.Forms.NumericUpDown();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numPublicationYear)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 13);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(58, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Название:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(100, 10);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(300, 20);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Location = new System.Drawing.Point(12, 39);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(43, 13);
+            this.lblAuthor.TabIndex = 2;
+            this.lblAuthor.Text = "Автор:";
+            // 
+            // cmbAuthors
+            // 
+            this.cmbAuthors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAuthors.FormattingEnabled = true;
+            this.cmbAuthors.Location = new System.Drawing.Point(100, 36);
+            this.cmbAuthors.Name = "cmbAuthors";
+            this.cmbAuthors.Size = new System.Drawing.Size(300, 21);
+            this.cmbAuthors.TabIndex = 3;
+            // 
+            // lblPublicationYear
+            // 
+            this.lblPublicationYear.AutoSize = true;
+            this.lblPublicationYear.Location = new System.Drawing.Point(12, 65);
+            this.lblPublicationYear.Name = "lblPublicationYear";
+            this.lblPublicationYear.Size = new System.Drawing.Size(74, 13);
+            this.lblPublicationYear.TabIndex = 4;
+            this.lblPublicationYear.Text = "Год издания:";
+            // 
+            // numPublicationYear
+            // 
+            this.numPublicationYear.Location = new System.Drawing.Point(100, 63);
+            this.numPublicationYear.Maximum = new decimal(new int[] { 9999, 0, 0, 0 });
+            this.numPublicationYear.Name = "numPublicationYear";
+            this.numPublicationYear.Size = new System.Drawing.Size(100, 20);
+            this.numPublicationYear.TabIndex = 5;
+            this.numPublicationYear.Value = new decimal(new int[] { 2023, 0, 0, 0 });
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(12, 91);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(59, 13);
+            this.lblDescription.TabIndex = 6;
+            this.lblDescription.Text = "Описание:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(100, 88);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(300, 100);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(325, 194);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // BookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 229);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.numPublicationYear);
+            this.Controls.Add(this.lblPublicationYear);
+            this.Controls.Add(this.cmbAuthors);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BookForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Добавить книгу";
+            this.Load += new System.EventHandler(this.BookForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numPublicationYear)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.ComboBox cmbAuthors;
+        private System.Windows.Forms.Label lblPublicationYear;
+        private System.Windows.Forms.NumericUpDown numPublicationYear;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ BookForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookRatingApp
+{
+    public partial class BookForm : Form
+    {
+        private readonly int? bookId;
+        private readonly DatabaseHelper dbHelper;
+        private DataTable authorsData;
+
+        public BookForm(DatabaseHelper dbHelper)
+        {
+            InitializeComponent();
+            this.dbHelper = dbHelper;
+            bookId = null;
+        }
+
+        public BookForm(int bookId, string title, int authorId, int? publicationYear, string description, DatabaseHelper dbHelper)
+        {
+            InitializeComponent();
+            this.bookId = bookId;
+            this.dbHelper = dbHelper;
+            txtTitle.Text = title;
+            numPublicationYear.Value = publicationYear ?? 2023;
+            txtDescription.Text = description;
+            btnSave.Text = "Обновить";
+            Text = "Редактирование книги";
+        }
+
+        private async void BookForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                authorsData = await dbHelper.GetAuthorsAsync();
+                cmbAuthors.DisplayMember = "full_name";
+                cmbAuthors.ValueMember = "id";
+                cmbAuthors.DataSource = authorsData;
+
+                if (bookId.HasValue)
+                {
+                    cmbAuthors.SelectedValue = authorsData.AsEnumerable()
+                        .FirstOrDefault(row => row.Field<int>("id") == (int)cmbAuthors.SelectedValue)?["id"];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки авторов: {ex.Message}");
+                Close();
+            }
+        }
+
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text.Trim();
+            int authorId = (int)cmbAuthors.SelectedValue;
+            int? publicationYear = numPublicationYear.Value > 0 ? (int?)numPublicationYear.Value : null;
+            string description = txtDescription.Text.Trim();
+
+            if (string.IsNullOrEmpty(title))
+            {
+                MessageBox.Show("Название книги не может быть пустым");
+                return;
+            }
+
+            try
+            {
+                if (bookId.HasValue)
+                {
+                    await dbHelper.UpdateBookAsync(bookId.Value, title, authorId, publicationYear, description);
+                }
+                else
+                {
+                    await dbHelper.AddBookAsync(title, authorId, publicationYear, description);
+                }
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка сохранения: {ex.Message}");
+            }
+        }
+    }
+}
